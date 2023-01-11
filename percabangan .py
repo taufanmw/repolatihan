1cfd3cdcbdb49bc175d45615e9a95d26f8411fea
LENOVO <taufanmuhamadwijaya@gmail.com>
2023-01-11 14:04:43 +0700

    perubahan repolatihan

---
+++ percabangan .py
@@ -0,0 +1,29 @@
+print('perintah ibu belanja di warung')
+jumlahbotolsusu = 173
+if jumlahbotolsusu > 0:
+    print("alhamdulillah ada barangnya")
+    print("Budi membeli barangnya")
+
+else :
+    print("Budi ga jadi beli susu")
+
+print('Budi pulang')
+print("Budi memberikan susu tsb kepada Ibu")
+
+print("-----------------------------------")
+
+jumlah_botol_susu = 173
+jumlah_telur = 1212
+print(f"jumlah botol susu {jumlah_botol_susu } botol " )
+print(f"jumlah telur adalah {jumlah_telur} butir")
+
+if jumlah_botol_susu>0:
+    print('Budi mengecek harga, ternyata cukup')
+    if jumlah_telur>0:
+        print('Budi membeli 1 botol susu')
+    else:
+        print('Budi beli 6  butir telur')
+else:
+    print('Budi ga jadi beli 1 botol susu')
+
+print('Budi pulang ke rumah')
---
+++ sekuensial.py
@@ -0,0 +1,2 @@
+print("halo ini tahap sekuensial")
+print('ini step kedua, "kedua" ')
